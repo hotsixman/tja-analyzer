777057c59d77ebcd598de9338ab4d05432e7351d
hotsixisbest <galactagun12@gmail.com>
2024-04-29 14:19:35 +0900

    .

--- src/class/Group.ts
+++ src/class/Group.ts
@@ -9,8 +9,6 @@ export default class Group {
     fraction: math.Fraction;
     delay: math.Fraction;
     notes: Note[] = [];//Types of Notes in Group Should Be 1|2|3|4
-    colorChanges: number = 0;
-    sizeChanges: number = 0;
 
     constructor(option: GroupOption) {
         this.bpm = option.bpm;
@@ -19,16 +17,6 @@ export default class Group {
         this.measure = option.measure;
         this.fraction = option.fraction;
         this.delay = option.delay;
-
-        this.notes.forEach((n, i, a) => {
-            if(i + 1 == a.length) return;
-            if((n.type < 3) !== (a[i+1].type < 3)){
-                this.sizeChanges++;
-            }
-            if((n.type % 2) !== (a[i+1].type % 2)){
-                this.colorChanges++;
-            }
-        })
     }
 }
 
